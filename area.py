d3cd715585782c6e4ce615454d2c94be74b5a648
deepansh4 <92592259+deepansh4@users.noreply.github.com>
2021-10-25 19:49:37 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,11 @@
+a=input("enter value of a ")
+a=int(a)
+b=input("enter value of b")
+b=int(b)
+c=a*b
+print("area of rectangle ",c)
+
+
+r=input("enter value of radius")
+r=int(r)
+print("area of cicle ",3.14*r*r)
